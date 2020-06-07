48d154118feb7a4bd1a152adb2200fd34d798f38
DESKTOP-UBERTA9\Sea Chan <thanhhaitran1404@gmail.com>
2020-06-08 00:14:09 +0700

    Update

---
+++ custom/js/skill.js
@@ -0,0 +1,25 @@
+window.skill = {
+	'skill' : {
+		'skill_atk_up' : 'Tăng ATK',
+		'skill_atk_down' : 'Giảm ATK',
+		'skill_def_up' : 'Tăng DEF',
+		'skill_def_down' : 'Giảm DEF',
+	},
+	'hidden_skill' : {
+		'hiddenskill_atk_up' : 'Tăng ATK',
+		'hiddenskill_atk_down' : 'Giảm ATK',
+		'hiddenskill_def_up' : 'Tăng DEF',
+		'hiddenskill_def_down' : 'Giảm DEF',
+	},
+	'ability' : {
+		'ability_prevent_para' : 'Ngăn tê liệt (paralysis)',
+		'ability_prevent_silence': 'Ngăn khóa chiêu',
+		'ability_prevent_crit': 'Ngăn Crit',
+		'ability_prevent_stack_dmg': 'Ngăn dmg cộng dồn',
+		'ability_heal': 'Hồi máu',
+	},
+	'target' : {
+		'target_nearby' : 'Ở gần',
+		'target_highest_atk' : 'ATK cao',
+	},
+}
